fe1209d38e84df9126c792aa3771de640bf5a0bc
Trang Lê <lethithutrang380@gmail.com>
2023-11-09 15:07:25 +0700

    feat: delete item in table

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,20 @@ interface UserProps {
 }
 
 //get users
+let totalUsers;
+let maxPage;
+const limit = 20;
+let skip = 0;
 
 const getDataUser = async () => {
   try {
-    const res = await fetch("https://dummyjson.com/users");
+    const res = await fetch(
+      `https://dummyjson.com/users?skip=${skip}&limit=${limit}`
+    );
     const data = await res.json();
     const users = data.users;
+    totalUsers = data.total;
+    maxPage = totalUsers / 20;
     displayDataUser(users);
     console.log("data", users);
   } catch (error) {
@@ -44,6 +52,9 @@ const displayDataUser = async (data: any) => {
           .join("/")}</td>
         <td class="table__desc table-todos">
         </td>
+        <td class="table__desc text-center justify-center"><button id="js-delete-${
+          r.id
+        }" class="btn btn-delete"><i class="fa-solid fa-trash"></i></button></td>
         `;
     tableTitleEle.appendChild(row);
 
@@ -62,8 +73,9 @@ const displayDataUser = async (data: any) => {
     const todoEle = parentElementTodo.querySelector(".table-todos")!;
     const listTodo = document.createElement("ul");
 
-    for (let todo of todosList.todos) {
-      listTodo.innerHTML += `
+    if (todosList.todos.length) {
+      for (let todo of todosList.todos) {
+        listTodo.innerHTML += `
       <li class="flex justify-between">
         <p>${todo.todo}</p>
         <span>
@@ -71,15 +83,38 @@ const displayDataUser = async (data: any) => {
         </span>
       </li>`;
 
+        todoEle.appendChild(listTodo);
+      }
+    } else {
+      listTodo.innerHTML = `<li>NO TODO </li>`;
       todoEle.appendChild(listTodo);
     }
+
+    // DELETE user
+    const btnDeleteEle = document.getElementById(`js-delete-${r.id}`);
+
+    btnDeleteEle?.addEventListener("click", async () => {
+      try {
+        const res = await fetch(`https://dummyjson.com/users/${r.id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (data.isDeleted) {
+          const elementDeleted = document.querySelector(`.table__tr${r.id}`);
+          elementDeleted?.remove();
+        }
+      } catch (error) {
+        console.log(error);
+        window.alert("not delete");
+      }
+    });
   }
 };
 
-// search user
+// SEARCH user
 
 const inputEle = document.getElementById("js-search") as HTMLInputElement;
-const btnSearchEle = document.getElementById("js-search-btn");
+const btnSearchEle = document.getElementById("js-search-btn")!;
 
 // call api for search
 
@@ -96,10 +131,35 @@ const getUserForSearch = async (q: string) => {
   }
 };
 
-btnSearchEle?.addEventListener("click", (e) => {
+btnSearchEle.addEventListener("click", (e) => {
   e.preventDefault();
   const valueInput = inputEle.value.trim();
 
   console.log("value", valueInput.split(" ")[0]);
   getUserForSearch(valueInput.split(" ")[0]);
 });
+
+// DROPDOWN gender
+
+const selectEle = document.getElementById(
+  "js-filter-gender"
+) as HTMLSelectElement;
+
+const getUserForFilterGender = async (value: string) => {
+  try {
+    const res = await fetch(
+      `https://dummyjson.com/users/filter?key=gender&value=${value}`
+    );
+    const data = await res.json();
+    displayDataUser(data?.users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+selectEle.addEventListener("change", () => {
+  console.log("value", selectEle.value);
+  getUserForFilterGender(selectEle.value);
+});
+
+// pagination
